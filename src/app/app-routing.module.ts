e8e73ee26f136c58c3841c1da88a90e07a5ab6d8
Robson Cunha <rcunha.sm@gmail.com>
2022-06-01 16:22:00 -0300

    AngularChartjs
    Ajuste deploy.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
   {
     path: "cidades", 
     loadChildren: () => import('./cidade/cidade-listar/cidade-listar.module').then(modulo => modulo.CidadeListarModule)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
