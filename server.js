4d304487e7b07e03065d26391aa6fadc6e5d7c16
Robson Cunha <rcunha.sm@gmail.com>
2022-06-01 16:50:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; // <== You can change the port
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
 const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
