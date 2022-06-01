17fb36f941ea236d105cf2039b8191ed89301f67
Robson Cunha <95380035+StrongRC@users.noreply.github.com>
2022-06-01 17:32:13 -0300

    Update cidade.service.ts

--- src/app/cidade/cidade.service.ts
+++ src/app/cidade/cidade.service.ts
@@ -8,7 +8,7 @@ import { Cidade } from './cidade.model';
 })
 export class CidadeService {
 
-  private baseUrl = "https://datasetdbjson.herokuapp.com/";
+  private baseUrl = "http://localhost:3000/";
   private endPoint = 'cidades';
 
   constructor(private httpClient: HttpClient) { }
